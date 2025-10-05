3732fd8423603f86b274dc9611fcea7ea34e705e
Rayhan Alam Towhid <rayhan.cse9007@baiust.ac.bd>
2025-10-04 20:32:44 -0700

    Create Selection-Sort-by-swap_function.cpp

---
+++ Selection-Sort-by-swap_function.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm> // for std::swap
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n - 1; i++){
+        for(int j = i + 1; j < n; j++){
+            if (arr[j] < arr[i]){
+                swap(arr[j], arr[i]); // Using the swap function from <algorithm>
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
